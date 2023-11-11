dc330b0f7f8b428d5b0db9f02dad81bc38e4ab22
Julio Juarez <juliojjuarezj@gmail.com>
2023-11-11 12:48:02 -0600

    first commit

---
+++ Estudiante.py
@@ -0,0 +1,7 @@
+class Estudiante:
+    nombre = ""
+    grado = ""
+    seccion = ""
+    telefono = 0
+def DatosEstudiante (self):
+    print("nombre:" , self.nombre , "grado:" , self.grado , "seccion:" , self.grado , "telefono:" , self.telefono)
